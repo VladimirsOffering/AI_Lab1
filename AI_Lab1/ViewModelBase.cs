68f12a305556fa5fa149a3c308df6702a6d4e91f
vladimir <qw3rty.asdd@yandex.ru>
2022-12-02 13:37:42 +0400

    new: Создан каркас проекта add: RelayCommand, ViewModelBase

---
+++ AI_Lab1/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace AI_Lab1
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
